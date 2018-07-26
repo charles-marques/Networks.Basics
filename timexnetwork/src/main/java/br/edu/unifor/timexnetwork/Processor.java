2744848a657ed14cb79064e09fde9e500c2b3cf3
Charles Marques <charles.marques@cge.ce.gov.br>
2018-07-26 17:15:01 -0300

    Adição do funcionalidade de estilo.

--- timexnetwork/src/main/java/br/edu/unifor/timexnetwork/Processor.java
+++ timexnetwork/src/main/java/br/edu/unifor/timexnetwork/Processor.java
@@ -11,7 +11,7 @@ public class Processor {
 	private static final String VERTICES = "Vértices:";
 	private static final String DEGREE = "Degree: ";
 	private static final String ID = "ID: ";
-	
+
 	public static void setNodesSizes(Graph graph, Integer escalaTamanho) {
 		int minimumsize = 1;
 		int maximumsize = graph.getNodeCount() * graph.getEdgeCount() * escalaTamanho;
@@ -27,7 +27,7 @@ public class Processor {
 			double scale = (double) (n.getDegree() - smaller) / (double) (greater - smaller);
 			if (null != n.getAttribute("ui.style")) {
 				n.setAttribute("ui.style", n.getAttribute("ui.style") + " size:"
-						+ Math.round((scale * maximumsize ) + minimumsize) + "px;");
+						+ Math.round((scale * maximumsize) + minimumsize) + "px;");
 			} else {
 				n.addAttribute("ui.style", " size:" + Math.round((scale * maximumsize) + minimumsize) + "px;");
 			}
@@ -35,6 +35,7 @@ public class Processor {
 	}
 
 	public static void main(String[] args) {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 		Graph graph = new SingleGraph("Tutorial 1");
 		graph.setStrict(false);
 		graph.addNode("A");
@@ -42,17 +43,28 @@ public class Processor {
 		graph.addNode("C");
 		graph.addNode("D");
 		graph.addEdge("AB", "A", "B", true);
-//		graph.addEdge("BC", "B", "C", true);
-//		graph.addEdge("CD", "C", "D", true);
+		graph.addEdge("BC", "B", "C", true);
+		graph.addEdge("CD", "C", "D", true);
 		graph.addEdge("DA", "D", "A", true);
 		graph.addEdge("CA", "C", "A", true);
 		graph.addEdge("BD", "B", "D", true);
 		graph.addEdge("BD2", "B", "D", true);
 		graph.addEdge("BD3", "B", "D", true);
 		graph.addEdge("BD4", "B", "D", true);
-		
-		setNodesSizes(graph, 5);
-		
+
+		setNodesSizes(graph, 2);
+
+//		Configurando Style
+		graph.getNode("A").setAttribute("ui.style", "shape:pie-chart;fill-color:rgb(127,0,55),rgb(255,0,110),rgb(1,127,1);");
+//		graph.getNode("A").setAttribute("ui.pie-values", "0.333,0.333,0.333");
+		graph.getNode("B").setAttribute("ui.style", "shape:pie-chart; fill-color: rgb(255,0,110), rgb(0,255,1);");
+//		graph.getNode("B").setAttribute("ui.pie-values", "0.5,0.5");
+		graph.getNode(3).setAttribute("ui.style", "shape:pie-chart; fill-color: rgb(255,0,110), rgb(0,255,1);");
+//		graph.getNode(3).setAttribute("ui.pie-values", "0.5,0.5");
+
+		graph.addAttribute("ui.quality");
+		graph.addAttribute("ui.antialias");
+		graph.addAttribute("ui.stylesheet", "url('resource/style.css')");
 		graph.display();
 
 		System.out.println(VERTICES);
